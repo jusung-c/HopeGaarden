e70ae8b0232ca9566241425740883c01514eedd0
이주성 <32183520@dankook.ac.kr>
2024-02-15 01:52:08 +0900

    멀티모듈 프로젝트 초기 세팅

---
+++ application/src/main/java/io/jus/hopegaarden/hopegaarden/HopeGaardenApplication.java
@@ -0,0 +1,13 @@
+package io.jus.hopegaarden.hopegaarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeGaardenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HopeGaardenApplication.class, args);
+    }
+
+}
---
+++ application/src/test/java/io/jus/hopegaarden/hopegaarden/HopeGaardenApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jus.hopegaarden.hopegaarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopeGaardenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
