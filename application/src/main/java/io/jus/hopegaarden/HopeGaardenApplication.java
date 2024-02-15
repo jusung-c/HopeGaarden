57f5b4746770858f7133eccfd00b451b95fc06a9
이주성 <32183520@dankook.ac.kr>
2024-02-15 17:26:07 +0900

    chore(#3): jasypt를 사용해 yml 파일 암호화

--- application/src/main/java/io/jus/hopegaarden/HopeGaardenApplication.java
+++ application/src/main/java/io/jus/hopegaarden/HopeGaardenApplication.java
@@ -1,4 +1,4 @@
-package io.jus.hopegaarden.hopegaarden;
+package io.jus.hopegaarden;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
