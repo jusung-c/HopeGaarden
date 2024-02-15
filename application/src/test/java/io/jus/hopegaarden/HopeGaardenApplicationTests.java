b3158638bc8d1074e9b9924d7ed3c5243676b817
이주성 <32183520@dankook.ac.kr>
2024-02-15 22:51:17 +0900

    chore(#3): TestConfig 작성 & jasypt 테스트

--- application/src/test/java/io/jus/hopegaarden/HopeGaardenApplicationTests.java
+++ application/src/test/java/io/jus/hopegaarden/HopeGaardenApplicationTests.java
@@ -3,8 +3,7 @@ package io.jus.hopegaarden;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class HopeGaardenApplicationTests {
+class HopeGaardenApplicationTests extends TestConfig {
 
     @Test
     void contextLoads() {
